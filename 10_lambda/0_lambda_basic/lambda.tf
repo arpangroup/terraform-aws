767fd9cf62e9599f2949fda1ea8811d65fa8698e
arpan jana <arpangroup1@gmail.com>
2025-01-09 20:58:11 +0530

    initial commit

--- 10_lambda/0_lambda_basic/lambda.tf
+++ 10_lambda/0_lambda_basic/lambda.tf
@@ -53,19 +53,9 @@ resource "aws_lambda_function_url" "my_lambda_url" {
   authorization_type = "NONE" # Public access (change to "AWS_IAM" for IAM-based access control)
 }
 
-# Fetch the current region
-data "aws_region" "current" {}
 
-/*output "function_details" {
-  value = aws_lambda_function.TF_LAMBDA_EXAMPLE
-}*/
-
-output "lambda_curl" {
-  value = <<-EOT
-    ..............................................
-    curl -X POST https://${aws_lambda_function_url.my_lambda_url.url_id}.lambda-url.${data.aws_region.current.name}.on.aws \
-      -H "Content-Type: application/json" \
-      -d '{"key1": "value1", "key2": "value2"}'
-    ..............................................
-  EOT
+resource "null_resource" "ZIP_LAMBDA" {
+  provisioner "local-exec" {
+    command = "echo CurrentDirectory: %CD% && zip ./lambda_function.zip ./lambda_function.py"
+  }
 }
