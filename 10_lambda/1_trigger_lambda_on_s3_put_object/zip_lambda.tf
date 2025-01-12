ba7c53f9e591f95f7176950e6db3d6a4fea3cf42
arpan jana <arpangroup1@gmail.com>
2025-01-13 00:34:31 +0530

    initial commit

--- 10_lambda/1_trigger_lambda_on_s3_put_object/zip_lambda.tf
+++
@@ -1,8 +0,0 @@
-# Create Zip file locally
-# zip ./lambda_function.zip ./lambda_function.py
-resource "null_resource" "ZIP_LAMBDA" {
-  provisioner "local-exec" {
-    command     = "echo CurrentDirectory: $(pwd) && zip ./lambda_function.zip ./lambda_function.py"
-    interpreter = ["C:\\Program Files\\Git\\bin\\bash.exe", "-c"]
-  }
-}
